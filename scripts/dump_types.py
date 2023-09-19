7366bfd42122ce20562fb5c403215f8a75a20d43
Aloroider <70312917+Aloroid@users.noreply.github.com>
2023-09-19 22:07:52 +0200

    update

--- scripts/dump_types.py
+++ scripts/dump_types.py
@@ -34,15 +34,13 @@ desired_classes = [
 
 lines_before = [
 	"type Prop<T> = (() -> T) | T?",
-	"type Event<T=() -> ()> = T?",
-	"type Array<T=any> = {T}",
-	"type Dictionary<T=any> = {[string]: T}"
+	"type Event<T=() -> ()> = T?"
 	""
 ]
 
 lines_after = [
 	""
-	"return \{\}"
+	"return {}"
 ]
 
 lines = []
@@ -51,10 +49,13 @@ for line in lines_before:
 	lines.append(line)
 
 API_DUMP_LINK = "https://raw.githubusercontent.com/MaximumADHD/Roblox-Client-Tracker/roblox/API-Dump.json"
+CORRECTIONS_LINK = "https://raw.githubusercontent.com/NightrainsRbx/RobloxLsp/master/server/api/Corrections.json"
 
 api_dump_request = requests.get(API_DUMP_LINK)
+corrections_dump_request = requests.get(CORRECTIONS_LINK)
 
 api_dump = api_dump_request.json()
+corrections_dump = corrections_dump_request.json()
 
 aliases = {
 	"int64": "number",
@@ -65,10 +66,14 @@ aliases = {
 	"Content": "string"
 }
 
+mapCorrections = {}
 mapRobloxClasses = {}
 
 for roblox_class in api_dump["Classes"]:
 	mapRobloxClasses[roblox_class["Name"]] = roblox_class
+
+for correction_class in corrections_dump["Classes"]:
+	mapCorrections[correction_class["Name"]] = correction_class
 
 def get_prop_type(value_type):
 
@@ -93,12 +98,18 @@ def get_prop_type(value_type):
 
 def append_class(roblox_class):
 	lines.append("\t-- " + roblox_class["Name"])
+	correction_class = mapCorrections.get(roblox_class["Name"]) or {"Members": []}
+	correction_members_map = {}
 
+	for member in correction_class["Members"]:
+		correction_members_map[member["Name"]] = member
+	
 	for member in roblox_class["Members"]:
 		if (member.get("Tags") and "ReadOnly" in member["Tags"]) == True: continue
 		if (member.get("Tags") and "Deprecated" in member["Tags"]) == True: continue
 		if (member.get("Tags") and "NotScriptable" in member["Tags"]) == True: continue
 		
+		# We check if it's a property and if it is, run the get_prop_type function.
 		if member["MemberType"] == "Property":
 			if member["Security"]["Write"] != "None": continue
 			
@@ -107,14 +118,35 @@ def append_class(roblox_class):
 			lines.append("\t" + member["Name"] + ": Prop<" + get_prop_type(member["ValueType"]) + ">;")
 		elif member["MemberType"] == "Event":
 			if member["Security"] != "None": continue
-			
+
+			correction_member = correction_members_map.get(member["Name"]) or {"Parameters": []}
+
+			correction_parameters_map = {}
+			for parameter in correction_member["Parameters"]:
+				correction_parameters_map[parameter["Name"]] = parameter
+
 			line = "\t" + member["Name"] + ": Event<("
 			is_first = True
 			for parameter in member["Parameters"]:
+
+				correction_parameter = correction_parameters_map.get(parameter["Name"])
+				
 				if is_first == False:
 					line += ", "
 				is_first = False
-				line += parameter["Name"] + ": " + get_prop_type(parameter["Type"])
+
+				line += parameter["Name"] + ": "
+
+				if correction_parameter == None:
+					line += get_prop_type(parameter["Type"])
+				else:
+					name = correction_parameter["Type"].get("Name")
+					generic = correction_parameter["Type"].get("Generic")
+
+					if name != None:
+						line += name
+					elif generic != None:
+						line += "{" + generic + "}"
 
 			line += ") -> ()>;"
 			lines.append(line)
@@ -125,9 +157,10 @@ def append_class(roblox_class):
 for class_name in desired_classes:
 	roblox_class = mapRobloxClasses[class_name]
 	name = roblox_class['Name']
-	lines.append("export type props" + name + " = {")
+	lines.append("export type " + name + " = {")
 	append_class(roblox_class)	
 	lines.append("}")
 
 dump = '\n'.join(lines)
 print(dump)
+print('\n'.join(lines_after))
